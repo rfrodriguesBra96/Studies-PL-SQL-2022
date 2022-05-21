ebb713d7a0d9f58d092c21d9d844746d99855178
Rodrigo Rodrigues <rodrigo.rodrigues@compasso.com.br>
2022-05-21 15:51:14 -0300

    secao-8: not_data_found no select

--- aula02.sql
+++ aula02.sql
@@ -21,5 +21,31 @@ BEGIN
     FROM employees
     -- Como alterou o schemma
     -- Acessa employee_id from rodrigo_rodrigues.employees
-    WHERE employee_id = vEmployee_id; 
+    WHERE employee_id = vEmployee_id;
+    DBMS_OUTPUT.PUT_LINE('vFirst_name IS ' || vFirst_name);
+    DBMS_OUTPUT.PUT_LINE('vLast_name IS ' || vLast_name);
+    DBMS_OUTPUT.PUT_LINE('vSalary IS ' || vSalary);
 END;
+
+-- Exceção NO_DATA_FOUND
+
+DECLARE
+    -- Variaveis para armazenar o retorno da query
+    -- Devem ter o mesmo tipo das colunas da tabela
+    -- Para isso usamos %type
+    vFirst_name employees.first_name%type;
+    vLast_name employees.last_name%type;
+    vSalary employees.salary%type;
+    vEmployee_id employees.employee_id%type := 50; -- procura por um empregado que não existe
+BEGIN
+    SELECT first_name, last_name, salary -- Colunas selecionadas
+    INTO vFirst_name, vLast_name, vSalary -- São armazendas dentro dessas variáveis
+    FROM employees
+    -- Como alterou o schemma
+    -- Acessa employee_id from rodrigo_rodrigues.employees
+    WHERE employee_id = vEmployee_id;
+    DBMS_OUTPUT.PUT_LINE('vFirst_name IS ' || vFirst_name);
+    DBMS_OUTPUT.PUT_LINE('vLast_name IS ' || vLast_name);
+    DBMS_OUTPUT.PUT_LINE('vSalary IS ' || vSalary);
+END;
+
