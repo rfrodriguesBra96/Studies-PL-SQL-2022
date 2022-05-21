77c470df7fc883177813c5ac1b7c2ce61e8d5884
Rodrigo Rodrigues <rodrigo.rodrigues@compasso.com.br>
2022-05-21 15:06:50 -0300

    secao-8: utilizando comandos sql

---
+++ aula01.sql
@@ -0,0 +1,6 @@
+-- O comando SELECT é usado para recuperar registros e popular variáveis
+
+-- Os comandos commit, savepoint e rollback controlam a transação
+
+-- Comandos DDL (create table, drop table, alter ..) não podem ser executados dentro do bloco PL/SQL
+
