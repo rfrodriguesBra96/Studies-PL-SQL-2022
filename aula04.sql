6d5d54113f51aea13cbb961e399c94b9549ae823
Rodrigo Rodrigues <rodrigo.rodrigues@compasso.com.br>
2022-05-21 17:05:00 -0300

    secao-8: update

---
+++ aula04.sql
@@ -0,0 +1,13 @@
+-- Usando o comando UPDATE
+
+ALTER SESSION SET CURRENT_SCHEMA=RODRIGO_RODRIGUES;
+
+SET SERVEROUTPUT ON;
+
+DECLARE
+
+BEGIN
+    UPDATE  employees
+    SET     first_name = 'Bianca'
+    WHERE   employee_id = 129;
+END;
